100c727d9647ee7a819f3df61599ce60d5a8dacb
John-Joseph Gray <jjofthegray@gmail.com>
2019-03-10 11:41:14 +0000

    Piotrs Edits

--- components/FileCreater.js
+++ components/FileCreater.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
+import FileSender from './components/FileSender';
 
 class FileSender extends Component{
+
+    // fromAccountID(accountID) {
+    //     const getTransactionsByAccount = id =>
+    //         require('axios')
+    //             .get(`https://api.todaqfinance.net/accounts/${id}/transactions?page=1&limit=1`, {
+    //                 headers: {
+    //                     'Content-Type': 'application/json',
+    //                     'x-api-key': '74be46bd-1135-429b-aeff-39204fa93310',
+    //                 },
+    //             })
+    //             .then(res => {
+    //                 console.log(res);
+    //                 localStorage.setItem("file", JSON.parse(JSON.stringify(res.data.included[2])));
+    //             })
+    //             .catch(error => console.log(error));
+    //
+    //
+    //     FileSender.transfer(accountID, '5fff2bda-9611-42fd-b63b-b35a82debce3', )
+    //
+    //     getTransactionsByAccount(accountID);
+    //
+    //
+    //     let index1 = localStorage.file.toString().indexOf("id")+5;
+    //     let index2 = localStorage.file.toString().indexOf("\"", index1);
+    //     return(localStorage.file.toString().substring(index1, index2));
+    // }
+
 
     create(name, height, weight, prescription, notes, initialaccount) {
         var transaction = {
