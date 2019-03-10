4175e4964d04c158cf392508a697b7a1993e3848
John-Joseph Gray <jjofthegray@gmail.com>
2019-03-10 10:01:35 +0000

    Added more fields to file creation

--- cupcake-ui/src/App.js
+++ cupcake-ui/src/App.js
@@ -1,56 +1,86 @@
 import React, { Component } from 'react';
-import './shared/styles/App.css';
-
-import { cupcakeBase } from './shared/constants/constants';
-
-import Bakery from './components/Bakery';
-import CupcakeTransfer from './components/CupcakeTransfer';
+import FileSender from './components/FileSender';
+import FileCreater from './components/FileCreater';
+import './shared/styles/PatientApp.css';
 
 class App extends Component {
   state = {
-    cupcake: cupcakeBase,
-    left: {
-      account: '', // FIXME
-      cupcakes: [],
-    },
-    right: {
-      account: '', // FIXME
-      cupcakes: [],
-    },
-  };
+      recipient: 'f7437823-2d26-47f8-a018-168e7cf10d7c',
+      sender: 'a2102dce-dcf2-43e1-aa8e-9b8eb07121ff',
+      file: 'e124eb86ff56fd6c2693bc0cbc529ddb88216d60130d8f305eb87f44fd9b66c0',
 
-  onBake = (cupcake) => {
-    console.log('Baking Cupcake', cupcake);
-  };
-
-  onTransfer = (fromAccount, toAccount, cupcake) => {
-    // console.log("Transfer Cupcake", fromAccount, toAccount, cupcake);
-  };
-
-  onSelected = (cupcake) => {
-    console.log('Selected', cupcake);
-
-    this.setState({
-      cupcake,
-    });
+      name: 'John',
+      height: '185',
+      weight: '75',
+      prescription: 'Anti-Malatia pills (x5)',
+      notes: 'None',
+      initialaccount: 'f7437823-2d26-47f8-a018-168e7cf10d7c',
   };
 
   render() {
-    const { left, right, cupcake } = this.state;
+    const { sender, recipient, file, name, height, weight, prescription, notes, initialaccount } = this.state;
 
     return (
-      <div className="container">
-        <h1>Cupcakes.OT</h1>
+        <div>
+          <h1>Transfer files</h1>
+          <br></br>
 
-        <CupcakeTransfer
-          left={left}
-          right={right}
-          onTransfer={this.onTransfer}
-          onCupcakeSelect={this.onSelected}
-        />
+          <div>Sender: <input
+              type="text"
+              value={this.state.sender}
+              onChange={evt => this.setState({ sender: evt.target.value })}
+          /></div>
+          <div>Recipient: <input
+              type="text"
+              value={this.state.recipient}
+              onChange={evt => this.setState({ recipient: evt.target.value })}
+          /></div>
+          <div id = "file"> File ID: <input
+              type="text"
+              // className="form-control"
+              value={this.state.file}
+              onChange={evt => this.setState({ file: evt.target.value })}
+          /></div>
+          {/* <div>Sender: {this.state.sender}</div>
+                <div>Recipient: {this.state.recipient}</div>
+                <div>File: {this.state.file}</div> */}
 
-        <Bakery cupcake={cupcake} onBake={this.onBake} onChange={this.onSelected} />
-      </div>
+          <FileSender
+              file={this.state.file}
+              sender={this.state.sender}
+              recipient={this.state.recipient}
+          /><br></br>
+
+          <h1> Create Patient Record </h1><br></br>
+
+          <div>Name: <input type="text"
+                            value={this.state.name}
+                            onChange={evt => this.setState({ name: evt.target.value })}/></div>
+          <div>Height: <input type="text"
+                              value={this.state.height}
+                              onChange={evt => this.setState({ height: evt.target.value })}/></div>
+          <div>Weight: <input type="text"
+                              value={this.state.weight}
+                              onChange={evt => this.setState({ weight: evt.target.value })}/></div>
+          <div>Prescription: <input type="text"
+                                    value={this.state.prescription}
+                                    onChange={evt => this.setState({ prescription: evt.target.value })}/></div>
+          <div>Notes: <input type="text"
+                             value={this.state.notes}
+                             onChange={evt => this.setState({ notes: evt.target.value })}/></div>
+          <div>Initial Account: <input type="text"
+                                       value={this.state.initialaccount}
+                                       onChange={evt => this.setState({ initialaccount: evt.target.value })}/></div>
+
+          <FileCreater
+              name = {this.state.name}
+              height = {this.state.height}
+              weight = {this.state.weight}
+              prescrption = {this.state.prescription}
+              notes = {this.state.notes}
+              initialaccount = {this.state.initialaccount}
+          />
+        </div>
     );
   }
 }
