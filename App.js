112c6cf959ea612045f219fac0d64c9fa830a554
Manu Varma <varmam@bxscience.edu>
2019-03-10 08:32:19 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FileSender from './components/FileSender';
 import FileCreater from './components/FileCreater';
+import './shared/styles/App.css';
 
 class App extends Component {
     state = {
@@ -8,41 +9,47 @@ class App extends Component {
         recipient: 'f7437823-2d26-47f8-a018-168e7cf10d7c',
         sender: 'a2102dce-dcf2-43e1-aa8e-9b8eb07121ff'
     };
-
-
-
     render() {
         const { sender, recipient, file } = this.state;
 
         return (
             <div>
-                <h1>Transfer files</h1>
+                <h1>Transfer files</h1><br></br>
 
-                <div><input
+                <div>Sender: <input
                     type="text"
                     value={this.state.sender}
                     onChange={evt => this.setState({ sender: evt.target.value })}
                 /></div>
-                <div><input
+                <div>Recipient: <input
                     type="text"
                     value={this.state.recipient}
                     onChange={evt => this.setState({ recipient: evt.target.value })}
                 /></div>
-                <div><input
+                <div id = "file"> File ID: <input
                     type="text"
-                    className="form-control"
+                    // className="form-control"
                     value={this.state.file}
                     onChange={evt => this.setState({ file: evt.target.value })}
                 /></div>
-                <div>Sender: {this.state.sender}</div>
+                {/* <div>Sender: {this.state.sender}</div>
                 <div>Recipient: {this.state.recipient}</div>
-                <div>File: {this.state.file}</div>
+                <div>File: {this.state.file}</div> */}
 
                 <FileSender
                     file={this.state.file}
                     sender={this.state.sender}
                     recipient={this.state.recipient}
-                />
+                /><br></br>
+
+                <h1> Create Patient Record </h1><br></br>
+
+                <div>Name: <input type="text"></input></div>
+                <div>Height: <input type="text"></input></div>
+                <div>Weight: <input type="text"></input></div>
+                <div>Perscriptions: <input type="text"></input></div>
+                <div>Notes: <input type="text"></input></div>
+                <div>Initial Account: <input type="text"></input></div>
 
                 <FileCreater
                     accountID = {this.state.sender}
